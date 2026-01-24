a147f0954eaf87036ec6c85a13aef8b0260f41e0
vishwaravi321 <vishwaravi321@gmail.com>
2026-01-24 20:05:05 +0530

    feat(fuel_theft): Fuel Theft alert on call

--- twilio_integration/twilio_integration/doctype/twilio_call_log/twilio_call_log.py
+++ twilio_integration/twilio_integration/doctype/twilio_call_log/twilio_call_log.py
@@ -450,3 +450,25 @@ def wallet_low_balance_url():
 	<Say>Thanks, Liquiconnect Team.</Say>
 	</Response>"""
     return Response(response, mimetype="text/xml")
+
+
+
+@frappe.whitelist(allow_guest=True)
+def fuel_theft_alert_url():
+    params = frappe.request.args
+
+    customer = params.get("customer")
+    vehicle_no = params.get("vehicle_no")
+    fuel_lost = params.get("fuel_lost")
+
+    response = f"""<?xml version="1.0" encoding="UTF-8"?>
+    <Response>
+        <Say>Dear {customer}, Greetings from Liquiconnect Team.</Say>
+        <Say>Fuel theft has been detected.</Say>
+        <Say>Vehicle number {vehicle_no}.</Say>
+        <Say>Approximate fuel loss is {fuel_lost} litres.</Say>
+        <Say>Please take immediate action.</Say>
+        <Say>Thank you. Liquiconnect Team.</Say>
+    </Response>"""
+
+    return Response(response, mimetype="text/xml")
